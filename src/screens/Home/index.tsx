25c96c4a0b9d78c89e5ab9440fb1556547ba8685
schirmann <cschirmann.dev@gmail.com>
2021-09-15 22:33:11 -0300

    iniciando repositorio

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import * as S from './styles';
+
+
+const Home = () => {
+  return (
+    <S.Container>
+      <S.Title>
+        Hello World Bitches
+      </S.Title>
+    </S.Container>
+  )
+}
+
+
+export default Home;
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,14 @@
+import { RFValue } from 'react-native-responsive-fontsize';
+import styled from 'styled-components/native';
+
+
+export const Container = styled.View`
+flex: 1;
+justify-content: center;
+align-items: center;
+`;
+
+export const Title = styled.Text`
+color: #f11;
+font-size: ${RFValue(15)}px;
+`;
