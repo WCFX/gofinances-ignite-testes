66924e38a07f99882834415db45bdaa308d641a2
schirmann <cschirmann.dev@gmail.com>
2021-09-16 15:30:48 -0300

    feat: envolvendo com ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
 import { NavigationContainer } from "@react-navigation/native";
 import Routes from "./routes";
+import theme from "./global/styles/theme";
 
 const App = () => {
   return (
+      <ThemeProvider theme={theme}>
     <NavigationContainer>
+  
       <Routes />
+    
     </NavigationContainer>
+      
+      </ThemeProvider>
   );
 };
 
