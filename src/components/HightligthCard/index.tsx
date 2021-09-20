2641f99b75cda8f979a5e72ad8d1a0a9fb075d18
schirmann <cschirmann.dev@gmail.com>
2021-09-20 19:47:08 -0300

    feat: criando componente

---
+++ src/components/HightligthCard/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import * as S from './styles';
+
+
+const HighlightCard = () => {
+  return (
+    <S.Container>
+
+    </S.Container>
+  )
+}
+
+
+export default HighlightCard;
