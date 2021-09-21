30cfaefc7b57cc2112a9d0cf51b4cd0bcfa3e5e4
schirmann <cschirmann.dev@gmail.com>
2021-09-20 22:37:31 -0300

    feat: criando os cards e style

--- src/components/HightligthCard/index.tsx
+++ src/components/HightligthCard/index.tsx
@@ -6,7 +6,15 @@ import * as S from './styles';
 const HighlightCard = () => {
   return (
     <S.Container>
-
+      <S.Header>
+        <S.Title>Entradas</S.Title>
+        <S.Icon name="arrow-up-circle"/>
+      </S.Header>
+      
+      <S.Footer>
+        <S.Amount>R$17.400,00</S.Amount>
+        <S.LastTransaction>Ultima entrada dia 15 de setembro.</S.LastTransaction>
+      </S.Footer>
     </S.Container>
   )
 }
--- src/components/HightligthCard/styles.ts
+++ src/components/HightligthCard/styles.ts
@@ -1,6 +1,32 @@
 import styled from "styled-components/native";
+import { Feather} from '@expo/vector-icons';
+import { RFValue } from "react-native-responsive-fontsize";
 
 
 export const Container = styled.View`
-flex-direction: row;
+background-color: ${({ theme }) => theme.colors.shape};
+width: ${RFValue(300)}px;
+border-radius: 25px;
+
+padding: 19px 23px;
+padding-bottom: ${RFValue(42)}px;
 `;
+
+export const Header = styled.View`
+`;
+
+export const Title = styled.Text``;
+
+export const Icon = styled(Feather)``;
+
+export const Footer = styled.View``;
+
+export const Amount = styled.Text`
+font-family: ${({ theme }) => theme.fonts.medium};
+font-size: ${RFValue(32)}px;
+color: ${({ theme }) => theme.colors.text_dark};
+
+margin-top: 38px;
+`;
+
+export const LastTransaction = styled.Text``;
