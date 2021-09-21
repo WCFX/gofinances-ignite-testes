2e007825955bcacf9695ca5a9c0b333ab84e6dcf
schirmann <cschirmann.dev@gmail.com>
2021-09-21 15:48:06 -0300

    feat: add icons e tipando componets cards

--- src/components/HightligthCard/index.tsx
+++ src/components/HightligthCard/index.tsx
@@ -2,18 +2,30 @@ import React from 'react';
 
 import * as S from './styles';
 
+interface Props {
+  title: string;
+  amount: string;
+  lastTransaction: string;
+  type: 'up' | 'down' | 'total'
+}
 
-const HighlightCard = () => {
+const icon = {
+  up: "arrow-up-circle",
+  down: "arrow-down-circle",
+  total: "dollar-sign"
+}
+
+const HighlightCard = ({type, title, amount, lastTransaction} : Props) => {
   return (
     <S.Container>
       <S.Header>
-        <S.Title>Entradas</S.Title>
-        <S.Icon name="arrow-up-circle"/>
+        <S.Title>{title}</S.Title>
+        <S.Icon name={icon[type]}/>
       </S.Header>
       
       <S.Footer>
-        <S.Amount>R$17.400,00</S.Amount>
-        <S.LastTransaction>Ultima entrada dia 15 de setembro.</S.LastTransaction>
+        <S.Amount>{amount}</S.Amount>
+        <S.LastTransaction>{ lastTransaction }</S.LastTransaction>
       </S.Footer>
     </S.Container>
   )
