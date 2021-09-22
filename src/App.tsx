fd1d40e1c43536f99bd1d69d9ee51397bbf6169b
schirmann <cschirmann.dev@gmail.com>
2021-09-22 16:24:22 -0300

    feat: melhorando identaçao

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
-import React from "react";
-import { ThemeProvider } from "styled-components";
-import { NavigationContainer } from "@react-navigation/native";
-import Routes from "./routes";
-import theme from "./global/styles/theme";
-import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from "@expo-google-fonts/poppins";
-import AppLoading from "expo-app-loading";
+import React from 'react';
 
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+import { NavigationContainer } from '@react-navigation/native';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
 
+import theme from './global/styles/theme';
+
+import Routes from './routes';
 
 const App = () => {
   const [fonstLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
-    Poppins_700Bold
+    Poppins_700Bold,
   });
   if (!fonstLoaded) {
-    return <AppLoading/>
+    return <AppLoading />;
   }
 
-
   return (
-      <ThemeProvider theme={theme}>
-    <NavigationContainer>
-  
-      <Routes />
-    
-    </NavigationContainer>
-      
-      </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <Routes />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 };
 
