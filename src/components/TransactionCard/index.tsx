91e3a8396b863f3a418214442f37d39bdf06a771
schirmann <cschirmann.dev@gmail.com>
2021-09-22 18:13:27 -0300

    feat e style: card lista criado

--- src/components/TransactionCard/index.tsx
+++ src/components/TransactionCard/index.tsx
@@ -2,10 +2,26 @@ import React from 'react';
 
 import * as S from './styles';
 
+// interface Props {
+//   title: string;
+//   amount: string;
+
+// }
+
 const TransactionCard = () => {
   return (
     <S.Container>
-      <S.Title />
+      <S.Title>Desenvolvimento de site</S.Title>
+      <S.Amount>R$12.000,00</S.Amount>
+
+      <S.Footer>
+        <S.Category>
+          <S.Icon name="dollar-sign" />
+          <S.CategoryName>Vendas</S.CategoryName>
+        </S.Category>
+
+        <S.Date>15/09/2020</S.Date>
+      </S.Footer>
     </S.Container>
   );
 };
