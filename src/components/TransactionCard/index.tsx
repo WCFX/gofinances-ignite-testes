d773e6552fd8c31ecafbfc507f77818ae376f050
schirmann <cschirmann.dev@gmail.com>
2021-09-22 16:23:13 -0300

    feat: criando estrutura da listagem

---
+++ src/components/TransactionCard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const TransactionCard = () => {
+  return (
+    <S.Container>
+      <S.Title />
+    </S.Container>
+  );
+};
+
+export default TransactionCard;
---
+++ src/components/TransactionCard/styles.ts
@@ -0,0 +1,6 @@
+import { Feather } from '@expo/vector-icons';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import styled from 'styled-components/native';
+
+export const Container = styled.View``;
+export const Title = styled.Text``;
