3465b49a394125c84274ed17cf9f89da62abb1a3
schirmann <cschirmann.dev@gmail.com>
2021-09-23 16:04:07 -0300

    chore: add nova rota e tela

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React from 'react';
 
-import { createNativeStackNavigator  } from '@react-navigation/native-stack'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import * as Views from '../screens';
 
-
-const { Navigator, Screen } = createNativeStackNavigator()
+const { Navigator, Screen } = createNativeStackNavigator();
 
 const Routes = () => {
   return (
-    <Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
-      <Screen  name="Home" component={Views.Home}/>
+    <Navigator
+      initialRouteName="Register"
+      screenOptions={{ headerShown: false }}
+    >
+      <Screen name="Home" component={Views.Home} />
+      <Screen name="Register" component={Views.Register} />
     </Navigator>
-  )
-}
+  );
+};
 
 export default Routes;
