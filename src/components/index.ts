66513c0546a1b62f5adaca83bfb8a37cd03c77f7
schirmann <cschirmann.dev@gmail.com>
2021-09-23 16:06:35 -0300

    chore: identando tela de rotas

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as HightlightCard } from './HightligthCard';
 export { default as TransactionCard } from './TransactionCard';
 export { default as Input } from './Forms/Input';
+export { default as Button } from './Forms/Button';
