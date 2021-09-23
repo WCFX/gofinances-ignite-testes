ec08bebd0f140a3729a1f0412796d9c8205713db
schirmann <cschirmann.dev@gmail.com>
2021-09-23 11:57:50 -0300

    feat: tipando transction card

--- src/components/TransactionCard/styles.ts
+++ src/components/TransactionCard/styles.ts
@@ -2,6 +2,10 @@ import { Feather } from '@expo/vector-icons';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
+interface TransactionProps {
+  type: 'positve' | 'negative';
+}
+
 export const Container = styled.View`
   background-color: ${({ theme }) => theme.colors.shape};
   border-radius: 20px;
@@ -15,9 +19,12 @@ export const Title = styled.Text`
   font-family: ${({ theme }) => theme.fonts.regular};
 `;
 
-export const Amount = styled.Text`
+export const Amount = styled.Text<TransactionProps>`
   font-family: ${({ theme }) => theme.fonts.regular};
   font-size: ${RFValue(20)}px;
+
+  color: ${({ theme, type }) =>
+    type === 'positive' ? theme.colors.sucess : theme.colors.attention};
   margin-top: 2px;
 `;
 
