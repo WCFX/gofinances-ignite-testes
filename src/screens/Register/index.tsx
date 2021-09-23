4be973e37db111fd4b188112292ea49b64ba5b0d
schirmann <cschirmann.dev@gmail.com>
2021-09-23 14:40:11 -0300

    feat: add nova tela de input

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import * as S from './styles';
+
+import Input from '../../components/Forms/Input';
+
+const Register = () => {
+  return (
+    <S.Container>
+      <S.Header>
+        <S.Title>Cadastro</S.Title>
+      </S.Header>
+      <S.Form>
+        <Input placeholder=" Nome" />
+        <Input placeholder="Preço" />
+      </S.Form>
+    </S.Container>
+  );
+};
+
+export default Register;
