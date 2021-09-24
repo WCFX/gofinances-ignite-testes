ef436559853b4aded959657ef5734e211021ec8b
schirmann <cschirmann.dev@gmail.com>
2021-09-23 21:27:40 -0300

    feat: add outcome e income a tela

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import * as S from './styles';
 
 import Button from '../../components/Forms/Button';
 import Input from '../../components/Forms/Input';
+import TransactionTypeButton from '../../components/Forms/TransactionTypeButton';
 
 const Register = () => {
+  const [transactiontype, setTransactiontype] = useState('');
+
+  function handleTransactionsTypeSelect(type: 'up' | 'down') {
+    setTransactiontype(type);
+  }
+
   return (
     <S.Container>
       <S.Header>
@@ -15,6 +22,20 @@ const Register = () => {
         <S.Fields>
           <Input placeholder=" Nome" />
           <Input placeholder="Preço" />
+          <S.TransactionsTypes>
+            <TransactionTypeButton
+              type="up"
+              title="Income"
+              onPress={() => handleTransactionsTypeSelect('up')}
+              isActive={transactiontype === 'up'}
+            />
+            <TransactionTypeButton
+              type="down"
+              title="Outcome"
+              onPress={() => handleTransactionsTypeSelect('down')}
+              isActive={transactiontype === 'down'}
+            />
+          </S.TransactionsTypes>
         </S.Fields>
 
         <Button title="Enviar" />
--- src/screens/Register/styles.ts
+++ src/screens/Register/styles.ts
@@ -29,3 +29,7 @@ export const Form = styled.View`
   padding: 24px;
 `;
 export const Fields = styled.View``;
+export const TransactionsTypes = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
