9ba551a50affba9dd8b9e87e9480e85747300725
schirmann <cschirmann.dev@gmail.com>
2021-09-25 15:20:20 -0300

    feat: inserindo listagem de categorias e botao

---
+++ src/screens/CategorySelect/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import { FlatList } from 'react-native-gesture-handler';
+
+import * as S from './styles';
+
+import Button from '../../components/Forms/Button';
+import { categories } from '../../utils/categories';
+
+interface Category {
+  key: string;
+  name: string;
+}
+
+interface Props {
+  category: string;
+  setCategory: (category: Category) => void;
+  closeSelectCategory: () => void;
+}
+
+const CategorySelect = ({
+  category,
+  setCategory,
+  closeSelectCategory,
+}: Props) => {
+  return (
+    <S.Container>
+      <S.Header>
+        <S.Title>Categoria</S.Title>
+      </S.Header>
+
+      <FlatList
+        data={categories}
+        style={{ flex: 1, width: '100%' }}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => (
+          <S.Category>
+            <S.Icon name={item.icon} />
+            <S.Name>{item.name}</S.Name>
+          </S.Category>
+        )}
+        ItemSeparatorComponent={() => <S.Separator />}
+      />
+      <S.Footer>
+        <Button title="Selecionar" />
+      </S.Footer>
+    </S.Container>
+  );
+};
+
+export default CategorySelect;
