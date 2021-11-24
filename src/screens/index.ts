d23283fdebc4065e17d56dde9f2ee776b101ef96
Cintia Schirmann <cintia.schirmann@gmail.com>
2021-11-24 18:20:38 -0300

    feat: inserindo tela de resumo

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,3 +1,4 @@
 export { default as Home } from './Home';
 export { default as Register } from './Register';
 export { default as CategorySelect } from './CategorySelect';
+export { default as Resume } from './Resume';
