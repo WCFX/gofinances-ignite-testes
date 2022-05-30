868f6f4ee8021b5a6b142845a68b3803eea61c38
Wagner Goulart <wcfx.dev@gmail.com>
2022-05-30 00:46:21 -0300

    chore: jest config and library installed

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'react-native',
+  testPathIgnorePatterns: ['/node_modules', '/android', '/ios'],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect',
+    'jest-styled-components',
+  ],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  ],
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
 
+import React from 'react';
 import { StatusBar } from 'react-native';
 
 import {
